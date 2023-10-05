fe6d6a56df7c2989bebe6496e7edbf0a9fc5f276
jvschamne <schamnejv@hotmail.com>
2023-10-05 17:54:49 -0300

    atualizacoes 5-10

--- Cliente.py
+++ Cliente.py
@@ -1,26 +1,147 @@
 import Pyro5.api
-Pyro5.config.SERIALIZER = 'marshal'
+import threading
+import inquirer
+
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+
+#Pyro5.api.config.SERIALIZER = 'marshal'
+
 # Classe que representa o cliente do sistema de gestão de estoque
 class StockManagementClient:
 
+    @Pyro5.api.expose 
     def notify_replenishment(self, product_code):
+        print("ok2")
         # Método chamado pelo servidor para notificar sobre a reposição de estoque
         print(f"Produto {product_code} atingiu o estoque mínimo. É necessário repor o estoque.")
 
+    @Pyro5.api.expose 
     def notify_unsold_products(self, product):
         # Método chamado pelo servidor para enviar relatórios de produtos não vendidos
         print(f"Produto {product['name']} ({product['code']}) não foi vendido.")
 
+
+def keysGenerator():
+
+    # Gerar um par de chaves RSA
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,  # Exponente público padrão (Fermat F4)
+        key_size=2048,          # Tamanho da chave (pode ser 2048, 3072, 4096, etc.)
+        backend=default_backend()
+    )
+
+    # Obter a chave pública correspondente
+    public_key = private_key.public_key()
+
+    # Serializar as chaves para armazenamento ou transmissão
+    private_key_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+    public_key_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    # Salvar as chaves em arquivos
+    with open("private_key.pem", "wb") as f:
+        f.write(private_key_pem)
+
+    with open("public_key.pem", "wb") as f:
+        f.write(public_key_pem)
+
+    print("Chaves geradas e salvas com sucesso!")
+    return [private_key_pem, public_key_pem, private_key]
+
+
+# Função para assinar uma mensagem com a chave privada
+def sign_message(message, private_key):
+    signature = private_key.sign(
+        message.encode('utf-8'),
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+
 # Configurar o cliente PyRO
 if __name__ == "__main__":
-    name = "NomeDoGestor"  # Nome do gestor de estoque
-    public_key = "ChavePublicaDoGestor"  # Chave pública do gestor de estoque
 
-    server_uri = "PYRONAME:stock_management_system"  # URI do servidor PyRO
+    # Registrar objeto cliente
+    daemon = Pyro5.api.Daemon()
+    uri = daemon.register(StockManagementClient())
+
+    # Gerar chaves
+    keys = keysGenerator()
+    private_key = keys[0]
+    public_key = keys[1]
+    pk = keys[2]
+
+    # Pegar objeto do servidor
+
+    servidor_nomes = Pyro5.api.locate_ns()
+    server_uri = servidor_nomes.lookup("stock_management_system")
 
 
-    with Pyro5.api.Proxy(server_uri) as server:
-        response = server.register_user(name,public_key)
-        print(response)
-
+    server = Pyro5.api.Proxy(server_uri) 
+    name = "NomeDoGestor2"  # Nome do gestor de estoque
+    response = server.register_user(name, public_key, uri)
+    print(response)
     # O cliente agora está registrado no servidor e pronto para receber notificações e relatórios
+        
+
+
+    #colocar request loop dentro de uma thread
+    threading.Thread(target=daemon.requestLoop).start()
+
+    message = name
+    signature = sign_message(message, pk)
+    
+    #while(True):
+    questions = [
+            inquirer.List('action', message="Qual acao deseja tomar?", 
+                          choices=['Entrada de produtos', 'Saida de produtos', 'Relatorio'],)
+
+    ]
+    answer = inquirer.prompt(questions)
+    print(answer)
+    if(answer['action'] == 'Entrada de produtos'):
+            print('Entrada de produtos')
+            server.record_entry(name, 1, 'bulacha', 'Bolacha Waffer', 10, 5.5, 50, signature)
+    elif(answer['action'] == 'Saida de produtos'):
+            print('Saida de produtos')
+            server.record_exit(1, name, 10, signature)
+    elif(answer['action'] == 'Relatorio'):
+            print('Relatorio')
+            questions2 = [
+            inquirer.List('action2', message="Qual acao deseja tomar?", 
+                          choices=['Produtos em estoque', 'Fluxo de movimentação', 'Lista de produtos sem saída'])
+
+             ]
+            answer = inquirer.prompt(questions2)
+            if(answer['action2']== 'Produtos em estoque'):
+                print('Produtos em estoque')
+                produtosEmEstoque= server.generate_stock_report('Produtos em estoque')
+                for product in produtosEmEstoque:
+                    print(f"Produto {product['name']} ({product['code']}) está acima do estoque mínimo possuindo {product['quantity']} unidades em estoque")
+
+            elif(answer['action2']== 'Fluxo de movimentação'):
+                    print('Fluxo de movimentação')
+                    fluxoMov= server.generate_stock_report('Fluxo de movimentação')
+                    print("Movimentos:")
+                    for product in fluxoMov:
+                        for movement in product['movements']:
+                            print(f"Produto {product['name']} ({product['code']})  - Tipo: {movement['type']}, Quantidade: {movement['quantity']}, Hora: {movement['time']}")
+
+            elif(answer['action2']== 'Lista de produtos sem saída'):
+                    print('Lista de produtos sem saída')
+                    prodSemSaida = server.generate_stock_report('Lista de produtos sem saída')
+                    for product in prodSemSaida :
+                        print(f"Produto {product['name']} ({product['code']}) não teve movimentos de saída até 2 minutos atrás.")
--- Servidor.py
+++ Servidor.py
@@ -1,9 +1,11 @@
 import Pyro5.api
-import hashlib
 import datetime
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
 
-Pyro5.config.SERIALIZER = 'marshal'
-# Classe que representa um produto no estoque
 class Product:
     def __init__(self, code, name, description, quantity, price, min_stock):
         self.code = code
@@ -35,9 +37,10 @@ class Product:
 
 # Classe que representa um usuário do sistema
 class User:
-    def __init__(self, name, public_key):
+    def __init__(self, name, public_key, client_object):
         self.name = name
         self.public_key = public_key
+        self.client_object = client_object
 
 # Classe que representa o sistema de gestão de estoque
 class StockManagementSystem:
@@ -46,38 +49,45 @@ class StockManagementSystem:
         self.products = {}  # Dicionário de produtos (código do produto -> objeto do produto)
         self.clients = {}  # Dicionário de clientes (nome do cliente -> objeto do cliente)
     @Pyro5.api.expose
-    def register_user(self, name, public_key):
+    def register_user(self, name, public_key, client_object):
+        print(public_key)
+        print("Usuarios cadastrados: ", self.users)
+        print("Name:", name)
         if name not in self.users:
-            user = User(name, public_key)
+            user = User(name, public_key, client_object)
             self.users[name] = user
+            print(self.users[name], self.users[name].name, self.users, self.users[name].public_key)
             return f"Usuário {name} registrado com sucesso."
         else:
+            print("else")
             return f"Usuário {name} já está registrado."
 
-    def add_product(self, code, name, description, quantity, price, min_stock):
-        if code not in self.products:
-            product = Product(code, name, description, quantity, price, min_stock)
-            self.products[code] = product
-            return f"Produto {name} ({code}) adicionado ao estoque."
-        else:
-            return f"Produto {name} ({code}) já está no estoque."
 
-    def record_entry(self, code, user_name, quantity, signature):
+    @Pyro5.api.expose
+    def record_entry(self, user_name, code, name, description, quantity, price, min_stock, signature):
         if user_name in self.users:
             user = self.users[user_name]
             if code in self.products:
+                print("Produto adicionado")
                 product = self.products[code]
                 # Verificar a assinatura digital com a chave pública do usuário
-                if self.verify_signature(signature, user.public_key):
+                if self.verify_signature(signature, user.public_key, user_name):
+                    print("Assinatura digital válida.")
                     product.add_entry(quantity)
                     # Verificar se a quantidade após a entrada atingiu o estoque mínimo
                     if product.quantity <= product.min_stock:
                         self.notify_replenishment(user_name, product)
                     return f"Entrada de {quantity} unidades de {product.name} registrada."
                 else:
+                    print("Assinatura digital inválida.")
                     return "Assinatura digital inválida."
             else:
-                return "Produto não encontrado."
+                print(name, code)
+                product = Product(code, name, description, quantity, price, min_stock)
+                self.products[code] = product
+                self.products[code].add_entry(quantity)
+                return f"Produto {name} ({code}) adicionado ao estoque."
+
         else:
             return "Usuário não encontrado."
 
@@ -87,7 +97,7 @@ class StockManagementSystem:
             if code in self.products:
                 product = self.products[code]
                 # Verificar a assinatura digital com a chave pública do usuário
-                if self.verify_signature(signature, user.public_key):
+                if self.verify_signature(signature, user.public_key, user_name):
                     product.add_exit(quantity)
                     return f"Saída de {quantity} unidades de {product.name} registrada."
                 else:
@@ -97,22 +107,112 @@ class StockManagementSystem:
         else:
             return "Usuário não encontrado."
 
-    def verify_signature(self, signature, public_key):
+    def verify_signature(self, signature, public_key, message):
         # Implemente a verificação da assinatura digital aqui
         # Use a chave pública para verificar a assinatura
         # Retorne True se a assinatura for válida, caso contrário, retorne False
+
+        # Decodifique a base64
+        public_key_bytes = base64.b64decode(public_key)
+
+        # Carregue a chave pública PEM
+        #public_key = serialization.load_pem_public_key(public_key_bytes, backend=default_backend())
+
+        print('tenta verificar assinatura:', message)
+        print(public_key)
+        try:
+            public_key.verify(
+                signature,
+                message.encode('utf-8'),
+                padding.PSS(
+                    mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.MAX_LENGTH
+                ),
+                hashes.SHA256()
+            )
+            return True  # Assinatura válida
+        except Exception as error:
+            print(error)
+            return False  # Assinatura inválida
+
+
+
         return True
     @Pyro5.api.expose
-    def generate_stock_report(self):
-        # Implemente a geração de relatórios aqui
-        # Isso pode incluir produtos em estoque, movimentação de estoque e produtos sem saída
-        pass
+    def generate_stock_report(self, report_type):
+        if report_type == 'Produtos em estoque':
+                emEstoque = []
+                for product in self.products.values():
+                     if product.quantity > product.min_stock:
+                        product_info = {
+                            "code": product.code,
+                            "name": product.name,
+                            "quantity": product.quantity
+                            }
+                        emEstoque.append(product_info)
+                return emEstoque
+    
+        elif report_type == 'Fluxo de movimentação':
+                current_time = datetime.datetime.now()
+                time = current_time - datetime.timedelta(minutes=1)
+           
+    
+                fluxoMov = []
+                for product in self.products.values():
+                     if product.quantity > product.min_stock:
+                        product_info = {
+                            "code": product.code,
+                            "name": product.name,
+                            "movements": []
+                            }
+
+                # Filtrar os movimentos que ocorreram até 2 minutos atrás
+                for movement_time, movement_type, movement_quantity in product.movements:
+                    if movement_time >= time:
+                        product_info["movements"].append({
+                            "time": movement_time,
+                            "type": movement_type,
+                            "quantity": movement_quantity
+                             })
+
+                fluxoMov.append(product_info)
+                return  fluxoMov
+                
+        elif report_type == 'Lista de produtos sem saída':
+                current_time = datetime.datetime.now()
+                two_minutes_ago = current_time - datetime.timedelta(minutes=1)
+
+                unsold_products = []
+
+                for product in self.products.values():
+                    has_exit_movements = any(
+                        movement_time >= two_minutes_ago and movement_type == "saída"
+                        for movement_time, movement_type, _ in product.movements
+                    )
+
+                    if not has_exit_movements:
+                        unsold_products.append({
+                            "code": product.code,
+                            "name": product.name
+                        })
+
+        return unsold_products
+
     @Pyro5.api.expose 
     def notify_replenishment(self, user_name, product):
         # Método para notificar o gestor quando um produto atinge o estoque mínimo
-        if user_name in self.clients:
-            client = self.clients[user_name]
-            client.notify_replenishment(product.code)
+        print("produto fora de esoque")
+        print(user_name, self.users)
+        if user_name in self.users:
+            print("user in clientes")
+            client = self.users[user_name]
+            print(client.client_object)
+
+            auxObject = Pyro5.api.Proxy(client.client_object)
+            print("ok1")
+            print(auxObject)
+            auxObject.notify_replenishment(product.code)
+            print("ok3")
     @Pyro5.api.expose
     def notify_unsold_products(self):
         # Método para enviar relatórios periódicos sobre produtos não vendidos
@@ -129,7 +229,7 @@ class StockManagementSystem:
 if __name__ == "__main__":
     daemon = Pyro5.api.Daemon()
     ns = Pyro5.api.locate_ns()
-    uri = daemon.register(StockManagementSystem)
+    uri = daemon.register(StockManagementSystem())
     ns.register("stock_management_system", uri)
     print("Servidor PyRO pronto.")
     daemon.requestLoop()
